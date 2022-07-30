bd963e64e0ab97c7b6d0ba51c1c3792df4afba6f
James Barker <james@jcbdevelopment.co.uk>
2022-07-30 05:27:51 +0100

    feat: temporary deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+nx deploy core --access public
+nx deploy from --access public
+nx deploy walker --access public
+nx deploy graphql --access public
+nx deploy prettier --access public
+nx deploy pull --access public
+nx deploy code --access public
+#nx deploy typescript --access public
